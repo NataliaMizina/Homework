2b4115b57045f1bd9761981ba15bf2b56355e17f
NataliaMizina <92439536+NataliaMizina@users.noreply.github.com>
2021-10-31 21:52:23 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -22,20 +22,18 @@ int main(int argc, char** argv) {
     numberOfPartitions = readNumberOfPartitions(argv[1]);
 
     if (numberOfPartitions>0) {
-        std::cout << "numberOfPartitions: " << numberOfPartitions << std::endl;
+
+        
 
         partitions = readFile(argv[1], h0, speed, numberOfPartitions);
-        std::cout << "h0: " << h0 << "   speed.vx: " << speed.vx << "   speed.vy: " << speed.vy << std::endl;
-
-        for (int i=0;i<numberOfPartitions;i++)
-            std::cout << "partitions["<< i+1 << "].x: " << partitions[i].x << "   partitions[" << i+1 << "].h: " << partitions[i].h << std::endl;
-        std::cout << "---------------------------------------" << std::endl;
+       
 
         int answer = MoveForward(h0, speed, partitions, startingDotX, numberOfPartitions, 0);
 
-        std::cout << "================================" << std::endl;
+
+   
         std::cout << "Dot landed in section: " << answer << std::endl;
-        std::cout << "================================" << std::endl;
+       
 
     }
     else
