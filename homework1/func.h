28fc416e116dfbea418f8bc39eccccb0e2a20e32
NataliaMizina <92439536+NataliaMizina@users.noreply.github.com>
2021-12-24 12:22:59 +0300

    Update func.h

--- homework1/func.h
+++ homework1/func.h
@@ -4,17 +4,17 @@
 #include <cmath>
 #include <exception>
 
-struct dot{
-    double x;
-    double y;
+struct Point{
+    int x;
+    int y;
 };
 
-struct ans
+struct Answer
 {
-    dot left;
-    dot right;
+    Point left;
+    Point right;
 };
 
-ans solution(dot* input, int n);
-dot* readFile(const char* path, int& n);
+Answer firstVar(Point* input, int n);
+Point* readFile(const char* path, int& n);
 
