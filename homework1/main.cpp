1b07a6eed85c791cfba2ce5b3610e0c1768ac065
NataliaMizina <92439536+NataliaMizina@users.noreply.github.com>
2021-12-24 12:23:21 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,5 +1,4 @@
 #include "func.h"
-#include <climits>
 
 using namespace std;
 
@@ -9,13 +8,26 @@ int main()
     int n = 0;
     try
     {
-        dot* dots = readFile("in.txt", n);
-        ans result = solution(dots, n);
+        Point* points = readFile("in.txt", n);
 
-        cout << "Leftmost: " << result.left.x << ' ' << result.left.y << '\n' <<
-             "Rightmost: " << result.right.x << ' ' << result.right.y << endl;
+        int maxLeftX=0;
+        int maxLeftY=0;
+        int maxRightX=0;
+        int maxRightY=0;
 
-        delete dots;
+        if (n>1)
+        {
+            Answer res = firstVar(points, n);
+            maxLeftX =res.left.x;
+            maxLeftY =res.left.y;
+            maxRightX =res.right.x;
+            maxRightY =res.right.y;
+        }
+
+        cout << "max left: " << maxLeftX<< ' ' << maxLeftY << '\n' <<
+             "max right: " << maxRightX << ' ' << maxRightY << endl;
+
+        delete points;
     }
     catch(std::runtime_error err)
     {
