038f5b6a55d4c469c20116cbc15bc0067ad6cee6
NataliaMizina <92439536+NataliaMizina@users.noreply.github.com>
2021-12-24 12:32:31 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -5,11 +5,11 @@
 
 int main(int argc, char** argv) {
     if (argc == 2) {
-        std::cout << "file name: " << argv[1] << std::endl;
+        //std::cout << "file name: " << argv[1] << std::endl;
     }
     else
     {
-        std::cout << "no arguments";
+        std::cout << "no arguments, or more than expected";
         return -1;
     }
 
@@ -22,23 +22,27 @@ int main(int argc, char** argv) {
     numberOfPartitions = readNumberOfPartitions(argv[1]);
 
     if (numberOfPartitions>0) {
-
-        
+       //std::cout << "numberOfPartitions: " << numberOfPartitions << std::endl;
 
         partitions = readFile(argv[1], h0, speed, numberOfPartitions);
-       
+       //std::cout << "h0: " << h0 << "   speed.vx: " << speed.vx << "   speed.vy: " << speed.vy << std::endl;
+
+
+/*        for (int i=0;i<numberOfPartitions;i++)
+            std::cout << "partitions["<< i+1 << "].x: " << partitions[i].x << "   partitions[" << i+1 << "].h: " << partitions[i].h << std::endl;
+        std::cout << "---------------------------------------" << std::endl;*/
 
         int answer = MoveForward(h0, speed, partitions, startingDotX, numberOfPartitions, 0);
 
-
-   
-        std::cout << "Dot landed in section: " << answer << std::endl;
-       
+        //std::cout << "================================" << std::endl;
+        //std::cout << "Dot landed in section: ";
+        std::cout << answer << std::endl;
+        //std::cout << "================================" << std::endl;
 
     }
     else
     {
-        std::cout << "Not enough partitions in the file";
+        std::cout << "0";
     }
     return 0;
 }
